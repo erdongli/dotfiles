2f5d2bf7b6f929db3401c305e49bd957574e698a
Erdong Li <1317183+erdongli@users.noreply.github.com>
2026-04-06 21:39:27 -0700

    feat(nvim): add more plugins

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,12 +1,79 @@
 return {
-  { -- gruvbox
+  {
+    -- gitsigns
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      signs = {
+        add = { text = "+" },
+        change = { text = "~" },
+        delete = { text = "_" },
+        topdelete = { text = "^" },
+        changedelete = { text = "~" },
+        untracked = { text = "|" },
+      },
+      current_line_blame = false,
+      on_attach = function(bufnr)
+        local gs = require("gitsigns")
+
+        local map = function(mode, lhs, rhs, desc)
+          vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+        end
+
+        map("n", "]h", function()
+          if vim.wo.diff then
+            vim.cmd.normal({ "]c", bang = true })
+          else
+            gs.nav_hunk("next")
+          end
+        end, "Next hunk")
+
+        map("n", "[h", function()
+          if vim.wo.diff then
+            vim.cmd.normal({ "[c", bang = true })
+          else
+            gs.nav_hunk("prev")
+          end
+        end, "Previous hunk")
+
+        map("n", "<leader>hp", gs.preview_hunk, "Preview hunk")
+        map("n", "<leader>hs", gs.stage_hunk, "Stage hunk")
+        map("n", "<leader>hr", gs.reset_hunk, "Reset hunk")
+        map("n", "<leader>hb", gs.blame_line, "Blame line")
+      end,
+    },
+  },
+
+  {
+    -- keybind suggestion
+    "folke/which-key.nvim",
+    event = "VimEnter",
+    ---@module "which-key"
+    ---@type wk.Opts
+    ---@diagnostic disable-next-line: missing-fields
+    opts = {
+      delay = 0,
+      icons = { mappings = vim.g.have_nerd_font },
+      spec = {
+        { "<leader>s", group = "[S]earch", mode = { "n", "v" } },
+        { "<leader>t", group = "[T]oggle" },
+        { "<leader>h", group = "Git [H]unk", mode = "n" },
+        { "gr", group = "LSP Actions", mode = "n" },
+      },
+    },
+  },
+
+  {
+    -- gruvbox
     "ellisonleao/gruvbox.nvim",
     priority = 1000,
-    config = function()
-      require("gruvbox").setup({
-        contrast = "soft",
-        transparent_mode = false,
-      })
+    opts = {
+      contrast = "soft",
+      transparent_mode = false,
+    },
+    config = function(_, opts)
+      require("gruvbox").setup(opts)
+      vim.o.background = "dark"
       vim.cmd.colorscheme("gruvbox")
     end,
   },
