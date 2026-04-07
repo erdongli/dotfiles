873138e97931f856929e5532c7085b67fd6754aa
Erdong Li <1317183+erdongli@users.noreply.github.com>
2026-04-06 21:59:22 -0700

    feat(nvim): add telescope

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -47,20 +47,102 @@ return {
   {
     -- keybind suggestion
     "folke/which-key.nvim",
-    event = "VimEnter",
-    ---@module "which-key"
-    ---@type wk.Opts
-    ---@diagnostic disable-next-line: missing-fields
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
     opts = {
       delay = 0,
       icons = { mappings = vim.g.have_nerd_font },
       spec = {
-        { "<leader>s", group = "[S]earch", mode = { "n", "v" } },
-        { "<leader>t", group = "[T]oggle" },
+        { "<leader>s", group = "[S]earch", mode = "n" },
+        { "<leader>t", group = "[T]oggle", mode = "n" },
         { "<leader>h", group = "Git [H]unk", mode = "n" },
         { "gr", group = "LSP Actions", mode = "n" },
       },
     },
+  },
+
+  {
+    -- fuzzy search
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        cond = function()
+          return vim.fn.executable("make") == 1
+        end,
+      },
+      "nvim-telescope/telescope-ui-select.nvim",
+      { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+    },
+
+    keys = {
+      { "<leader>sh", "<cmd>Telescope help_tags<cr>", desc = "[S]earch [H]elp" },
+      { "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "[S]earch [K]eymaps" },
+      { "<leader>sf", "<cmd>Telescope find_files<cr>", desc = "[S]earch [F]iles" },
+      { "<leader>ss", "<cmd>Telescope builtin<cr>", desc = "[S]earch [S]elect Telescope" },
+      { "<leader>sw", "<cmd>Telescope grep_string<cr>", desc = "[S]earch current [W]ord" },
+      { "<leader>sg", "<cmd>Telescope live_grep<cr>", desc = "[S]earch by [G]rep" },
+      { "<leader>sd", "<cmd>Telescope diagnostics<cr>", desc = "[S]earch [D]iagnostics" },
+      { "<leader>sr", "<cmd>Telescope resume<cr>", desc = "[S]earch [R]esume" },
+      { "<leader>s.", "<cmd>Telescope oldfiles<cr>", desc = "[S]earch Recent Files" },
+      { "<leader>sc", "<cmd>Telescope commands<cr>", desc = "[S]earch [C]ommands" },
+      { "<leader><leader>", "<cmd>Telescope buffers<cr>", desc = "Find existing buffers" },
+
+      {
+        "<leader>/",
+        function()
+          require("telescope.builtin").current_buffer_fuzzy_find(
+            require("telescope.themes").get_dropdown({
+              winblend = 10,
+              previewer = false,
+            })
+          )
+        end,
+        desc = "[/] Fuzzily search in current buffer",
+      },
+
+      {
+        "<leader>s/",
+        function()
+          require("telescope.builtin").live_grep({
+            grep_open_files = true,
+            prompt_title = "Live Grep in Open Files",
+          })
+        end,
+        desc = "[S]earch [/] in Open Files",
+      },
+
+      {
+        "<leader>sn",
+        function()
+          require("telescope.builtin").find_files({
+            cwd = vim.fn.stdpath("config"),
+          })
+        end,
+        desc = "[S]earch [N]eovim files",
+      },
+    },
+
+    config = function()
+      local telescope = require("telescope")
+      local themes = require("telescope.themes")
+
+      telescope.setup({
+        extensions = {
+          ["ui-select"] = {
+            themes.get_dropdown(),
+          },
+        },
+      })
+
+      pcall(telescope.load_extension, "fzf")
+      pcall(telescope.load_extension, "ui-select")
+    end,
   },
 
   {
